b6bfa77ae28549cc271a8b33a95ec2c8780103ef
MhelDeLeon <mheldeleonoriginal@gmail.com>
2026-03-31 18:49:29 +0800

    Trial Upload

---
+++ _files/init/files.js
@@ -0,0 +1,42 @@
+﻿var files_json = {
+	length:6
+	,
+	imagescount:58
+	,
+	nonseq:
+		[
+			{"":"js/player.js"}
+		]
+	,
+	seq:
+		{
+			yahoo:
+			{
+				
+			}
+			,
+			jquery:
+			{
+				scripts:
+	[
+		{"":"js/jq/jquery-1.6.2.min.js"}
+		,
+		{"":"js/jq/jquery.ui.core.js"}
+		,
+		{"":"js/jq/jquery.ui.widget.js"}
+		,
+		{"":"js/jq/jquery.ui.mouse.js"}
+		,
+		{"":"js/jq/jquery.ui.slider.js"}
+	]
+	,
+css :
+	[
+		{"":"js/jq/demos.css"}
+		,
+		{"":"js/jq/jquery.ui.all.css"}	
+	]
+			}
+		}
+
+}
